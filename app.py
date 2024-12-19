9a003a77b55f667ff901f72a190c83093129a847
ashwinch22b025 <133302763+ashwinch22b025@users.noreply.github.com>
2024-12-19 23:25:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,92 +1,101 @@
-from bs4 import BeautifulSoup
+import json
 import requests
-import pandas as pd
 import streamlit as st
 
-def getData(userName):
-    url = "https://github.com/{}?tab=repositories".format(userName)
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content , 'html.parser')
-    info = {}
+st.title("GitHub User, Repo, and Commit Info")
 
-    # Scraping Profile Info
+# Input username
+userName = st.text_input("Enter GitHub Username")
 
-    #full Name
-    info['name'] = soup.find(class_ = 'vcard-fullname').get_text()
-    #image
-    info['image_url'] =  soup.find(class_ = 'avatar-user')['src']
-    #followers and follwoing
-    info['followers'] = soup.select_one("a[href*=followers]").get_text().strip().split('\n')[0]
-    info['following'] = soup.select_one("a[href*=following]").get_text().strip().split('\n')[0]
-    
-    #location
-    try:
-        info['location'] = soup.select_one('li[itemprop*=home]').get_text().strip()
-    except:
-        info['location'] = ''
-    #url
-    try:
-        info['url'] = soup.select_one('li[itemprop*=url]').get_text().strip()
-    except:
-        info['url'] = ''
-    
-    #get Repositories as a dataframe
-    repos = soup.find_all(class_ = 'source')
-    repo_info = []
-    for repo in repos:
+if userName:
+    st.subheader("User Info")
 
-        #repo name and link
-        try:
-            name = repo.select_one('a[itemprop*=codeRepository]').get_text().strip()
-            link = 'https://github.com/{}/{}'.format(userName,name)
-        except:
-            name = ''
-            link = ''
+    # User Info from User class
+    class User:
+        def __init__(self, Username):
+            self.Username = Username
+            self.UserURL = f'https://api.github.com/users/{self.Username}'
         
-        #repo update time
-        try:
-            updated = repo.find('relative-time').get_text()
-        except:
-            updated = ''
+        def get_user_stats(self):
+            UserDataFromGithub = requests.get(self.UserURL).json()
+            DataNeeded = [
+                'name',
+                'type',
+                'company',
+                'blog',
+                'location',
+                'email',
+                'public_repos',
+                'followers'
+            ]
+            self.UserData = {k: v for k, v in UserDataFromGithub.items() if k in DataNeeded}
+            return self.UserData
+
+    user = User(userName)
+    user_data = user.get_user_stats()
+    st.json(user_data)
+
+    st.subheader("Repositories Info")
+
+    # Repository Info from Repo class
+    class Repo:
+        def __init__(self, username):
+            self.username = username
+            self.repo_url = f'https://api.github.com/users/{self.username}/repos'
         
-        # programming language
-        try:
-            language = repo.select_one('span[itemprop*=programmingLanguage]').get_text()
-        except:
-            language = ''
+        def get_all_repos(self):
+            repos = requests.get(self.repo_url).json()
+            repo_stats = []
+            DataNeeded = [
+                'name',
+                'html_url',
+                'description',
+                'forks',
+                'open_issues',
+                'language',
+                'git_url',
+            ]
+            for repo in repos:
+                repo_data = {k: repo.get(k, 'N/A') for k in DataNeeded}
+                repo_stats.append(repo_data)
+            return repo_stats
+
+    repo = Repo(userName)
+    all_repos = repo.get_all_repos()
+    for idx, repo_data in enumerate(all_repos, start=1):
+        st.write(f"Repository {idx}")
+        st.json(repo_data)
+
+    st.subheader("Commit Info")
+
+    # Commit Info from Commit class
+    class Commit:
+        def __init__(self, username, project_id, sha):
+            self.username = username
+            self.project_id = project_id
+            self.sha = sha
+            self.commit_url = f'https://api.github.com/repos/{self.username}/{self.project_id}/commits/{self.sha}'
         
-        # description
-        try:
-            description = repo.select_one('p[itemprop*=description]').get_text().strip()
-        except:
-            description = ''
-        
-        repo_info.append({'name': name ,
-        'link': link , 
-        'updated ':updated , 
-        'language': language , 
-        'description':description})
-    repo_info = pd.DataFrame(repo_info)
-    return info , repo_info
-st.title("Github Scraper")
+        def get_commit_stats(self):
+            commit_data = requests.get(self.commit_url).json()
+            return {
+                'committer': commit_data['commit']['committer'],
+                'commit': commit_data['commit'],
+                'message': commit_data['commit']['message']
+            }
 
-userName = st.text_input('Enter Github Username')
-
-if userName != '':
-    try:
-        info, repo_info = getData(userName)
-
-        for key , value in info.items():
-            if key != 'image_url':
-                st.subheader(
-                    '''
-                    {} : {} 
-                    '''.format(key, value)
-                )
-            else:
-                st.image(value)
-        st.subheader(" Recent Repositories")
-        st.table(repo_info)
-    except:
-        st.subheader("User doesn't exist")
-
+    # Fetch commits for the first repository as an example
+    if all_repos:
+        first_repo_name = all_repos[0]['name']
+        st.write(f"Fetching commits for repository: {first_repo_name}")
+        repo_url = f'https://api.github.com/repos/{userName}/{first_repo_name}/commits'
+        commits = requests.get(repo_url).json()
+        if commits:
+            for commit in commits[:5]:  # Limit to 5 commits for display
+                st.json({
+                    'SHA': commit.get('sha'),
+                    'Message': commit['commit']['message'],
+                    'Author': commit['commit']['author'],
+                })
+else:
+    st.write("Please enter a GitHub username.")
